ef22b63df4de797d0f34b1e5a4cad77e83ecb117
pgarcia1000 <pgarcia1000@github.com>
2021-03-12 14:10:17 -0600

    mi segundo commit

---
+++ app/Console/Commands/Update.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\Install\UpdateCopied;
+use App\Events\Install\UpdateDownloaded;
+use App\Events\Install\UpdateUnzipped;
+use App\Jobs\Install\CopyFiles;
+use App\Jobs\Install\DownloadFile;
+use App\Jobs\Install\FinishUpdate;
+use App\Jobs\Install\UnzipFile;
+use App\Traits\Jobs;
+use App\Utilities\Versions;
+use Illuminate\Console\Command;
+
+class Update extends Command
+{
+    use Jobs;
+
+    const CMD_SUCCESS = 0;
+
+    const CMD_ERROR = 1;
+
+    public $alias;
+
+    public $company;
+
+    public $new;
+
+    public $old;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update {alias} {company} {new=latest}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Allows to update Akaunting and modules directly through CLI';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        set_time_limit(3600); // 1 hour
+
+        $this->alias = $this->argument('alias');
+        $this->company = $this->argument('company');
+
+        if (false === $this->new = $this->getNewVersion()) {
+            $this->error('Not able to get the latest version of ' . $this->alias . '!');
+
+            return self::CMD_ERROR;
+        }
+
+        $this->old = $this->getOldVersion();
+
+        session(['company_id' => $this->company]);
+        setting()->setExtraColumns(['company_id' => $this->company]);
+
+        if (!$path = $this->download()) {
+            return self::CMD_ERROR;
+        }
+
+        if (!$this->unzip($path)) {
+            return self::CMD_ERROR;
+        }
+
+        if (!$this->copyFiles($path)) {
+            return self::CMD_ERROR;
+        }
+
+        if (!$this->finish()) {
+            return self::CMD_ERROR;
+        }
+
+        return self::CMD_SUCCESS;
+    }
+
+    public function getNewVersion()
+    {
+        return ($this->argument('new') == 'latest') ? Versions::latest($this->alias) : $this->argument('new');
+    }
+
+    public function getOldVersion()
+    {
+        if ($this->alias == 'core') {
+            return version('short');
+        }
+
+        if ($module = module($this->alias)) {
+            return $module->get('version');
+        }
+
+        return '1.0.0';
+    }
+
+    public function download()
+    {
+        $this->info('Downloading update...');
+
+        try {
+            $path = $this->dispatch(new DownloadFile($this->alias, $this->new));
+
+            event(new UpdateDownloaded($this->alias, $this->new, $this->old));
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+
+            return false;
+        }
+
+        return $path;
+    }
+
+    public function unzip($path)
+    {
+        $this->info('Unzipping update...');
+
+        try {
+            $this->dispatch(new UnzipFile($this->alias, $path));
+
+            event(new UpdateUnzipped($this->alias, $this->new, $this->old));
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function copyFiles($path)
+    {
+        $this->info('Copying update files...');
+
+        try {
+            $this->dispatch(new CopyFiles($this->alias, $path));
+
+            event(new UpdateCopied($this->alias, $this->new, $this->old));
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function finish()
+    {
+        $this->info('Finishing update...');
+
+        try {
+            $this->dispatch(new FinishUpdate($this->alias, $this->new, $this->old, $this->company));
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Document',
+
+    'major'     =>  '2',
+
+    'minor'     =>  '1',
+
+    'patch'     =>  '7',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '10-March-2020',
+
+    'time'      =>  '19:00',
+
+    'zone'      =>  'GMT +3',
+
+];
